064980d9d888805398f386df56734af0aafd3848
anyone <anyone@localhost>
2022-03-31 18:05:04 +0900

    #로그인 완성(자동로그인제외)

--- src/main/java/org/baseball/kia/controller/LoginController.java
+++ src/main/java/org/baseball/kia/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.baseball.kia.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,27 +17,29 @@ public class LoginController {
 	@Autowired
 	AccountService accountService;
 
-	@RequestMapping("/login")
-	public String loginHandle() {
+	@GetMapping("/login")
+	public String loginGetHandle() {
 		return "taejeong/account/login";
 	}
+	
 	
 	@RequestMapping("/login/accountFind")
 	public String accountFindHandle() {
 		return "taejeong/account/accountFind";
 	}
 
-	@PostMapping("/session")
-	public String sessionHandle(HttpSession session, @RequestParam String loginId, @RequestParam String loginPass, Model model) {
-		boolean valid = accountService.isValidAccount(loginId, loginPass);
+	@PostMapping("/login")
+	public String loginPostHandle(HttpSession session, @RequestParam String id, @RequestParam String pw, Model model) {
+		boolean valid = accountService.isValidAccount(id, pw);
 		if (valid) {
-			AccountVo vo = accountService.findAccountById(loginId);
+			AccountVo vo = accountService.findAccountById(id);
 			// vo == null ?
 			session.setAttribute("loginUser", vo);
 			return "redirect:/";
 		} else {
-			model.addAttribute("err", "error");
-			return "login";
+			model.addAttribute("loginErrMsg", "아이디가 없거나 비밀번호가 일치하지 않습니다.");
+			System.out.println("로그인불가");
+			return "taejeong/account/login";
 		}
 	}
 	
--- src/main/webapp/WEB-INF/views/taejeong/signup/signup.jsp
+++ src/main/webapp/WEB-INF/views/taejeong/signup/signup.jsp
@@ -27,21 +27,6 @@
 	integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
 	crossorigin="anonymous">
 
-<!-- Favicons -->
-<!-- <link rel="apple-touch-icon"
-	href="/docs/5.1/assets/img/favicons/apple-touch-icon.png"
-	sizes="180x180">
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png"
-	sizes="32x32" type="image/png">
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png"
-	sizes="16x16" type="image/png">
-<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
-<link rel="mask-icon"
-	href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg"
-	color="#7952b3">
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
-<meta name="theme-color" content="#7952b3"> -->
-
 
 <style>
 .bd-placeholder-img {
@@ -74,6 +59,7 @@
 	height: 33px;
 	margin: 8px 0;
 }
+
 </style>
 
 
@@ -82,7 +68,6 @@
 <body>
 
 	<main>
-	<h1 class="visually-hidden">Heroes examples</h1>
 	<div class="b-example-divider"></div>
 
 	<div class="container col-xl-10 col-xxl-8 px-4 py-5">
