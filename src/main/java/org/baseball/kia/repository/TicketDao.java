f97eb9ca4c53d7a808657d1b940d791a64f2ab80
LEE EUNJI <eunji6986@naver.com>
2022-04-07 01:48:59 +0900

    #22-04-07 # TicketService 작성중

--- src/main/java/org/baseball/kia/repository/TicketDao.java
+++ src/main/java/org/baseball/kia/repository/TicketDao.java
@@ -3,6 +3,7 @@ package org.baseball.kia.repository;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.baseball.kia.entity.BaseballVo;
 import org.baseball.kia.entity.TicketVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -14,15 +15,15 @@ public class TicketDao {
 	SqlSession sqlSession;
 	
 	
-	public List<BaseballVo> seatTable() {
-		return sqlSession.selectList("ticket.seatTable");
+	public List<BaseballVo> seatPriceTable() {
+		return sqlSession.selectList("ticket.seatPriceTable");
 	}
 	
 	public List<TicketVo> showGameList() {
 		return sqlSession.selectList("ticket.showGameList");
 	}
 	
-	public TicketVo selectOne(int no) {
+	public TicketVo selectGame(int no) {
 		return sqlSession.selectOne("ticket.selectGame",no);
 	}
 	
--- src/main/java/org/baseball/kia/service/TicketService.java
+++ src/main/java/org/baseball/kia/service/TicketService.java
@@ -2,6 +2,7 @@ package org.baseball.kia.service;
 
 import java.util.List;
 
+import org.baseball.kia.entity.BaseballVo;
 import org.baseball.kia.entity.TicketVo;
 import org.baseball.kia.repository.TicketDao;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,13 @@ public class TicketService {
 	@Autowired
 	TicketDao ticketDao; 
 	
-	//ticketMain
+	//zoneInfo
+	public List<BaseballVo> seatPriceTable() {
+		List<BaseballVo> list = ticketDao.seatPriceTable();
+		return list;
+	}
+	
+	//ticketBuy
 	public List<TicketVo> showGameList(){
 		List<TicketVo> list = ticketDao.showGameList();
 		return list; 
